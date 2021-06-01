2fa081b7b3f131e131e4ea132a27f0604fe246fd
Antonio Barandica <anjose.121@gmail.com>
2021-06-01 13:16:04 -0500

    Initial commit Add lambdas

---
+++ translate.py
@@ -0,0 +1,27 @@
+import boto3
+import json
+import os
+import urllib.request
+# SOURCELANGUAGECODE	en
+# TARGETLANGUAGECODE	es
+translate_client = boto3.client('translate')
+
+def lambda_handler(event, context):
+    payload = event['Input']['Payload']
+    transcriptFileUri = payload['TranscriptFileUri']
+    transcriptionJobName = payload['TranscriptionJobName']
+
+    transcriptFile = urllib.request.urlopen(transcriptFileUri).read()
+    transcript = json.loads(transcriptFile)
+    transcript_text = transcript['results']['transcripts'][0]['transcript']
+
+    response = translate_client.translate_text(
+        Text=transcript_text,
+        SourceLanguageCode=os.environ['SOURCELANGUAGECODE'],
+        TargetLanguageCode=os.environ['TARGETLANGUAGECODE']
+    )
+
+    return {
+        'TranslatedText': response['TranslatedText'],
+        'TranscriptionJobName': transcriptionJobName,
+    }
